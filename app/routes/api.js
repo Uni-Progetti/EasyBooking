10cf85b7bfa4911ab60ced91bb997d4baee3ceaa
michela-fuselli <fuselli.1883535@studenti.uniroma1.it>
2022-09-06 19:06:11 +0200

    aggiunte chiamate api per dipartimenti, spazi, posti e tipologie

--- app/routes/api.js
+++ app/routes/api.js
@@ -9,7 +9,7 @@ const amqpUrl = process.env.AMQP_URL || 'amqp://localhost:5673';
 var amqp = require('amqplib/callback_api');
 const dayjs = require('dayjs');
 
-// stampa dipartimenti
+// stampa tutti i dipartimenti
 router.get('/getDepartments/all', function(req, res){
     const get_options = {
         hostname: 'couchdb',
@@ -40,10 +40,125 @@ router.get('/getDepartments/all', function(req, res){
     });
 
     usrs.end();
+});
+
+
+// stampa tutti gli spazi
+router.get('/getSpaces/all', function(req, res){
+    const get_options = {
+        hostname: 'couchdb',
+        port: 5984,
+        path: '/db/_design/Space/_view/0_All_Spaces',
+        method: 'GET',
+        auth: process.env.COUCHDB_USER+":"+process.env.COUCHDB_PASSWORD
+    };
+
+    var data = "";
+    const usrs = http.request(get_options, out => {
+        console.log(`statusCode: ${out.statusCode}`);
+        out.setEncoding('utf8');
+        out.on('data', d => {
+            data += d.toString();
+            //process.stdout.write(d);
+        });
+        out.on('end', function() {
+            var x = JSON.parse(data);
+            console.log(x);
+            res.status(200).json(x);
+        });
+    });
+
+    usrs.on('error', error => {
+        console.log(error);
+        res.status(503);
+    });
+
+    usrs.end();
+});
+
+
+// stampa tutti gli spazi per tipologia
+router.get('/getSpaces/:typology', function(req, res){
+    const get_options = {
+        hostname: 'couchdb',
+        port: 5984,
+        path: '/db/_design/Space/_view/0_All_Spaces',
+        method: 'GET',
+        auth: process.env.COUCHDB_USER+":"+process.env.COUCHDB_PASSWORD
+    };
+
+    var data = "";
+    const usrs = http.request(get_options, out => {
+        console.log(`statusCode: ${out.statusCode}`);
+        out.setEncoding('utf8');
+        out.on('data', d => {
+            data += d.toString();
+            //process.stdout.write(d);
+        });
+        out.on('end', function() {
+            var x = JSON.parse(data);
+            console.log(x);
+            var output = {};
+            x.rows.forEach(element => {
+                if (element.value.typology == req.params.typology) {
+                    output [element.key] = element.value; 
+                }
+            });
+            const isEmpty = Object.keys(output).length === 0;
+            if (isEmpty) {
+                res.status(404).send({message: "Nessun elemento trovato per la tipologia " + req.params.typology});
+            }
+            else {
+                res.status(200).json(output);
+            }
+        });
+    });
+
+    usrs.on('error', error => {
+        console.log(error);
+        res.status(503);
+    });
+
+    usrs.end();
+});
+
+
+// stampa tutti i posti
+router.get('/getSeats/all', function(req, res){
+    const get_options = {
+        hostname: 'couchdb',
+        port: 5984,
+        path: '/db/_design/Seat/_view/0_All_Seats',
+        method: 'GET',
+        auth: process.env.COUCHDB_USER+":"+process.env.COUCHDB_PASSWORD
+    };
+
+    var data = "";
+    const usrs = http.request(get_options, out => {
+        console.log(`statusCode: ${out.statusCode}`);
+        out.setEncoding('utf8');
+        out.on('data', d => {
+            data += d.toString();
+            //process.stdout.write(d);
+        });
+        out.on('end', function() {
+            var x = JSON.parse(data);
+            console.log(x);
+            res.status(200).json(x);
+        });
+    });
+
+    usrs.on('error', error => {
+        console.log(error);
+        res.status(503);
+    });
+
+    usrs.end();
 })
 
 
-// stampa posti dipartimento
+// stampa tutti i posti di un dato spazio
+
 // localizza dipartimento
 // stampa le mie prenotazioni
 // elimina prenotazioni
