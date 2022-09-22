8f15fda35db5c4b27162bd8866262161fb55b760
michela-fuselli <fuselli.1883535@studenti.uniroma1.it>
2022-09-22 15:41:36 +0200

    implementata la desincronizzazione di una prenotazione da calendar

--- app/routes/personalArea.js
+++ app/routes/personalArea.js
@@ -82,14 +82,13 @@ router.post('/sync_events', function(req, res) {
 	var ress = JSON.parse(req.body.reservation);
 
 	if(!(ress.value.fields.is_sync)) {
-
 		console.log(ress.value);
 		console.log('dip : ', ress.value.fields.dep_name + ' - ' + ress.value.fields.typology + ' ' + ress.value.fields.space_name);
 		console.log('inizio : ', ress.value.fields.start_date);
 		console.log('fine : ', ress.value.fields.end_date);
 
 		const postData = JSON.stringify({
-			'summary': `${ress.value.fields.dep_name+': '+ress.value.fields.typology+' - '+ress.value.fields.space_name}`,
+			'summary': `${ress.value.fields.dep_name + ' - ' + ress.value.fields.typology + ' ' + ress.value.fields.space_name}`,
 			'start': { 
 				'dateTime': `${String(ress.value.fields.start_date.Y)+'-'+((String(ress.value.fields.start_date.M).length == 1)? "0"+String(ress.value.fields.start_date.M):String(ress.value.fields.start_date.M))+'-'+((String(ress.value.fields.start_date.D).length == 1)? "0"+String(ress.value.fields.start_date.D):String(ress.value.fields.start_date.D))+'T'+((String(ress.value.fields.start_date.h).length == 1)? "0"+String(ress.value.fields.start_date.h):String(ress.value.fields.start_date.h))+':00:00'}`,
 				'timeZone': 'Europe/Rome',
@@ -118,6 +117,7 @@ router.post('/sync_events', function(req, res) {
 			});
 			out.on('end', function() {
 				var x = JSON.parse(data);
+				console.log('----------------------', x);
 				res.header("Content-Type",'application/json');
 				// aggiorna il campo is_sync della prenotazione
 				const to_sync_postData = JSON.stringify({
@@ -146,7 +146,7 @@ router.post('/sync_events', function(req, res) {
 							"m": parseInt(ress.value.fields.end_date.m),
 							"s": parseInt(ress.value.fields.end_date.s)
 						},
-						"is_sync": true,
+						"is_sync": String(x.id),
 						"state": "Active"
 					},
 					"_rev": ress.value.rev
@@ -166,52 +166,120 @@ router.post('/sync_events', function(req, res) {
 		usrs.write(postData);
 		usrs.end();
 	}
+
+	else {
+		const post_options = {
+			hostname: 'www.googleapis.com',
+			port: 443,
+			path: '/calendar/v3/calendars/primary/events/'+ress.value.fields.is_sync,
+			method: 'DELETE',
+			headers: { Authorization: `Bearer ${access_token}` },
+		};
+
+		var data = "";
+		const usrs = https.request(post_options, out => {
+			console.log(`statusCode: ${out.statusCode}`);
+			out.setEncoding('utf8');
+			out.on('data', d => {
+				data += d.toString();
+				//process.stdout.write(d);
+			});
+			out.on('end', function() {
+				// var x = JSON.parse(data);
+				console.log('----------------------', data, '***********************');
+				res.header("Content-Type",'application/json');
+				// aggiorna il campo is_sync della prenotazione
+				const to_desync_postData = JSON.stringify({
+					"key": ress.id,
+					"type": "Reservation",
+					"fields": {
+						"email": ress.value.fields.email,
+						"dep_name": ress.value.fields.dep_name,
+						"typology": ress.value.fields.typology,
+						"space_name": ress.value.fields.space_name,
+						"seat_id": ress.value.fields.seat_id,
+						"seat_number": ress.value.fields.position,
+						"start_date": {
+							"Y": parseInt(ress.value.fields.start_date.Y),
+							"M": parseInt(ress.value.fields.start_date.M),
+							"D": parseInt(ress.value.fields.start_date.D),
+							"h": parseInt(ress.value.fields.start_date.h),
+							"m": parseInt(ress.value.fields.start_date.m),
+							"s": parseInt(ress.value.fields.start_date.s)
+						},
+						"end_date": {
+							"Y": parseInt(ress.value.fields.end_date.Y),
+							"M": parseInt(ress.value.fields.end_date.M),
+							"D": parseInt(ress.value.fields.end_date.D),
+							"h": parseInt(ress.value.fields.end_date.h),
+							"m": parseInt(ress.value.fields.end_date.m),
+							"s": parseInt(ress.value.fields.end_date.s)
+						},
+						"is_sync": false,
+						"state": "Active"
+					},
+					"_rev": ress.value.rev
+				});
+				couchdb_utils.post_to_couchdb('/db/'+ress.id, to_desync_postData, function(err, response) {
+					if (err) {console.log(err)}
+					else { res.redirect('/personalArea') }
+				})
+			});
+		});
+
+		usrs.on('error', error => {
+			console.log(error);
+			res.status(503);
+		});
+
+		usrs.end();
+	};
 });
 
-module.exports = router;
+
 // POST remove reservation + decrease seat position
 router.post('/rm_res', function(req, res){
-  // Delete reservation
-  couchdb_utils.delete_from_couchdb('/db/'+req.body.res_id, function(err, response) {
-    if (err) {console.log(err)}
-    else {/*console.log("RESERVATION DELETED: "+req.body.res_id)*/}
-  })
-  // GET seat
-  couchdb_utils.get_from_couchdb('/db/'+req.body.seat_id, function(err, seat_response) {
-    if (err) { console.log(err) }
-    else { var seat = seat_response 
-      // Decrease seat position
-      const decrease_st_position_postData = JSON.stringify({
-        "key": seat.key,
-        "type": "Seat",
-        "fields": {
-          "position": (seat.fields.position)-1,
-          "start_date": {
-            "Y": seat.fields.start_date.Y,
-            "M": seat.fields.start_date.M,
-            "D": seat.fields.start_date.D,
-            "h": seat.fields.start_date.h,
-            "m": seat.fields.start_date.m,
-            "s": seat.fields.start_date.s
-          },
-          "end_date": {
-            "Y": seat.fields.end_date.Y,
-            "M": seat.fields.end_date.M,
-            "D": seat.fields.end_date.D,
-            "h": seat.fields.end_date.h,
-            "m": seat.fields.end_date.m,
-            "s": seat.fields.end_date.s
-          },
-          "state": "Active"
-        },
-        "_rev": seat._rev
-      });
-      couchdb_utils.post_to_couchdb('/db/'+seat._id, decrease_st_position_postData, function(err, response) {
-        if (err) {console.log(err)}
-        else { res.redirect('/personalArea') }
-      })
-    }
-  })
+	// Delete reservation
+	couchdb_utils.delete_from_couchdb('/db/'+req.body.res_id, function(err, response) {
+		if (err) {console.log(err)}
+		else {/*console.log("RESERVATION DELETED: "+req.body.res_id)*/}
+	})
+	// GET seat
+	couchdb_utils.get_from_couchdb('/db/'+req.body.seat_id, function(err, seat_response) {
+		if (err) { console.log(err) }
+		else { var seat = seat_response 
+			// Decrease seat position
+			const decrease_st_position_postData = JSON.stringify({
+				"key": seat.key,
+				"type": "Seat",
+				"fields": {
+					"position": (seat.fields.position)-1,
+					"start_date": {
+						"Y": seat.fields.start_date.Y,
+						"M": seat.fields.start_date.M,
+						"D": seat.fields.start_date.D,
+						"h": seat.fields.start_date.h,
+						"m": seat.fields.start_date.m,
+						"s": seat.fields.start_date.s
+					},
+					"end_date": {
+						"Y": seat.fields.end_date.Y,
+						"M": seat.fields.end_date.M,
+						"D": seat.fields.end_date.D,
+						"h": seat.fields.end_date.h,
+						"m": seat.fields.end_date.m,
+						"s": seat.fields.end_date.s
+					},
+					"state": "Active"
+				},
+				"_rev": seat._rev
+			});
+			couchdb_utils.post_to_couchdb('/db/'+seat._id, decrease_st_position_postData, function(err, response) {
+				if (err) {console.log(err)}
+				else { res.redirect('/personalArea') }
+			})
+		}
+	})
 })
 
 module.exports = router;
